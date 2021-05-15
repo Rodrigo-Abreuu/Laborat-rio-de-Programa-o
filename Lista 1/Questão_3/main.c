594106acbe5a35b9346ab22a3739df480fe90c63
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-05-15 10:44:50 -0300

    Questão 3

---
+++ Lista 1/Questão_3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float valor;
+    printf("Digite o valor: ");
+    scanf("%f", &valor);
+    printf("O número digitado foi %.1f", valor);
+    return 0;
+}
