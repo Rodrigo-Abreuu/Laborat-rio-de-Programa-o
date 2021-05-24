4f178706efc533488a6b684963a3791be1f63734
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-05-23 21:23:03 -0300

    Lista 2 - Questão 2

---
+++ Lista_2/Questão_2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char dia;
+
+    do{
+        printf("Digite o numero do dia da semana: ");
+        scanf(" %c", &dia);
+
+        switch(dia){
+            case('1'):{
+                printf("Domingo\n");
+                 dia = 'a';
+                 break;}
+
+            case('2'):{
+                printf("Segunda\n");
+                 dia = 'a';
+                 break;}
+
+            case('3'):{
+                printf("Terça\n");
+                dia = 'a';
+                break;}
+
+            case('4'):{
+                printf("Quarta\n");
+                dia = 'a';
+                break;}
+
+            case('5'):{
+                printf("Quinta\n");
+                dia = 'a';
+                break;}
+
+            case('6'):{
+                printf("Sexta\n");
+                dia = 'a';
+                break;}
+
+            case('7'):{
+                printf("Sábado\n");
+                dia = 'a';
+                break;}
+
+            case('0'):{ break;}
+
+            default:{ printf("Número de dia não válido\n"); dia = 'a';}
+        }
+    }while(dia == 'a');
+    return 0;
+}
