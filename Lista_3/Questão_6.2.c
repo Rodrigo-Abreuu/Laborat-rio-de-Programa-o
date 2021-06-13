8cb235579a501f918f1483f7c804b48cd988b8ad
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-06-12 22:50:39 -0300

    Questão 6 e 6.2 - Lista 3

---
+++ Lista_3/Questão_6.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 10
+
+int main()
+{
+    char palavra1[TAM], palavra2[TAM];
+
+    printf("Digite a primeira palavra: ");
+    gets(palavra1);
+    printf("Digite a segunda palavra: ");
+    gets(palavra2);
+
+    printf("%s%s", palavra1, palavra2);
+    return 0;
+}
