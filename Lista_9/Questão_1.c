6807d237a8e594463abb7a24b21c599d4df9edf6
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-09-12 12:39:31 -0300

    Questão 1 - Lista 9

---
+++ Lista_9/Questão_1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    while(1){
+        int apoio = 0;
+        char nome[30] = "";
+        char telefone[15] = "";
+        printf("-Lista telefonica\n");
+        printf("  -=# Menu #=-\n");
+        printf("[1] - Adicionar contato\n");
+        printf("[2] - Sair\n>>");
+        scanf("%d", &apoio);
+        getchar();
+
+        switch(apoio){
+            case 1:
+            printf("Digite o nome do contato: ");
+            gets(nome);
+            printf("Digite o telefone do contato: ");
+            gets(telefone);
+
+            cadastrarContato(nome, telefone);
+            break;
+
+            case 2:
+            exit(0);
+            break;
+        }
+    }
+    return 0;
+}
+
+void cadastrarContato(char nome[], char telefone[]){
+    FILE *arquivo;
+    arquivo = fopen("arquivo.csv", "a");
+    fprintf(arquivo, "%s,%s,", nome, telefone);
+    fclose(arquivo);
+}
